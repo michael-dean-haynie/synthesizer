533597ebe666488dd172e8f8bb13cdc4920aa288
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2021-04-09 09:52:03 -0400

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SynthesizeComponent } from './components/synthesize/synthesize.component';
+
+
+const routes: Routes = [
+  { path: 'synthesize', component: SynthesizeComponent },
+  { path: '', redirectTo: '/synthesize', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SynthesizeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/synthesize/synthesize.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-synthesize',
+  templateUrl: './synthesize.component.html',
+  styleUrls: ['./synthesize.component.css']
+})
+export class SynthesizeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
